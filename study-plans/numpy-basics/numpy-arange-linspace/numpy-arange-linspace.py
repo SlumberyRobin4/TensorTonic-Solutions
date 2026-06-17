453b3a8496893eb0156d412f0a14cfc8302f339d
SlumberyRobin4 <128005711+SlumberyRobin4@users.noreply.github.com>
2026-06-17 15:39:11 -0400

    Solve numpy-arange-linspace

---
+++ study-plans/numpy-basics/numpy-arange-linspace/numpy-arange-linspace.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def create_sequence(start, stop, param, kind):
+    """
+    Returns: 1D ndarray of float64 values
+    """
+    if kind == 'arange':
+        return np.arange(start, stop, param, dtype = 'float64')
+    else:
+        return np.linspace(start, stop, param)
